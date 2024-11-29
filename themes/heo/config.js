b9d70d097f16dc5242da30a4524cba530778cd87
NaNQiQ <167856378+NaNQiQ@users.noreply.github.com>
2024-11-29 21:46:12 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,14 +7,14 @@ const CONFIG = {
   HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
+  //HEO_NOTICE_BAR: [
   
-  ],
+  //],
 
   // 英雄区左右侧组件颠倒位置
-  HEO_HERO_REVERSE: false,
+  //HEO_HERO_REVERSE: false,
   // 博客主体区左右侧组件颠倒位置
-  HEO_HERO_BODY_REVERSE: false,
+  //HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
   //HEO_HERO_TITLE_1: '分享编程',
