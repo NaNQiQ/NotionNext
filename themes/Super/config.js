4e1df83b6daa2c7a41c794d01bb214ff0b2c8060
NaNQiQ <167856378+NaNQiQ@users.noreply.github.com>
2024-12-24 17:49:52 +0800

    Add files via upload

---
+++ themes/Super/config.js
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  title: 'Personal Blog',
+  description: 'A personal blog built with Next.js',
+  author: 'Syakir',
+  social: {
+    github: 'https://github.com'
+  }
+}
+
