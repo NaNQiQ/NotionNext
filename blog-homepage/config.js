e10158c2305dd47bad2d1b426e8ebfdd57897068
NaNQiQ <167856378+NaNQiQ@users.noreply.github.com>
2024-12-24 17:48:08 +0800

    Delete blog-homepage directory
    Delete blog-homepage directory

--- blog-homepage/config.js
+++
@@ -1,9 +0,0 @@
-export const siteConfig = {
-  title: 'Personal Blog',
-  description: 'A personal blog built with Next.js',
-  author: 'Syakir',
-  social: {
-    github: 'https://github.com'
-  }
-}
-
